d0f8452fe72ab82bd212fd7142ac887c9eaa690a
Matt Goucher <matt@mattgoucher.com>
2016-04-17 02:07:55 -0700

    Add strava configuration

--- config/production.js
+++ config/production.js
@@ -1,3 +1,9 @@
 module.exports = {
-    port: process.env.PORT
+    port: process.env.PORT,
+
+    /**
+     * Strava Configuration
+     */
+    strava_id: process.env.STRAVA_ID,
+    strava_secret: process.env.STRAVA_SECRET
 };
