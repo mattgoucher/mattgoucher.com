8826b491db77e092d98b8cbd816a60e8a9e51b4a
Matt Goucher <matt@mattgoucher.com>
2016-04-17 15:36:39 -0700

    Production env configuration

--- config/index.js
+++ config/index.js
@@ -6,18 +6,18 @@
  */
 module.exports = (function _config() {
   let config;
-  let {port = 3000, env = 'development'} = process.env;
+  let {port = 3000, NODE_ENV = 'development'} = process.env;
 
   // Try loading the config..
   try {
-    config = require(`./${env}`);
-    config.env = env;
+    config = require(`./${NODE_ENV}`);
+    config.env = NODE_ENV;
     config.port = config.port || port;
   } catch (e) {
-    console.log(`Unable to load '${env}' configuration.`);
+    console.log(`Unable to load '${NODE_ENV}' configuration.`);
     return process.exit(0);
   }
 
-  console.log(`Configuration '${env}' loaded.`);
+  console.log(`Configuration '${NODE_ENV}' loaded.`);
   return config;
 })();
--- config/production.js
+++ config/production.js
@@ -10,5 +10,8 @@ module.exports = {
    * Strava Configuration
    */
   strava_id: process.env.STRAVA_ID,
-  strava_secret: process.env.STRAVA_SECRET
+  strava_athlete_id: process.env.STRAVA_ATHLETE_ID,
+  strava_athlete_bike: process.env.STRAVA_ATHLETE_BIKE,
+  strava_secret: process.env.STRAVA_SECRET,
+  strava_access_token: process.env.STRAVA_ACCESS_TOKEN
 };
