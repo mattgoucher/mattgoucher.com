e2a7926deffda906ac6fd09cf968c862eee99837
Matt Goucher <matt@mattgoucher.com>
2016-05-30 18:55:52 -0700

    circle version

---
+++ build/version.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+git config --global user.email "matt@mattgoucher.com"
+git config --global user.name "mattgoucher"
+
+  standard-version patch -m "chore(release): version %s build ${CIRCLE_BUILD_NUM} [ci skip]"
+
+  git push --tags origin ${CIRCLE_BRANCH}
+
+  npm publish
