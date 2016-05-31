0926083501197e0f6eba7406ea6d88121e7e024b
mattgoucher <matt@mattgoucher.com>
2016-05-30 19:31:51 -0700

    npm 3.9.3

--- build/version.sh
+++ build/version.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-git config --global user.email "matt@mattgoucher.com"
-git config --global user.name "mattgoucher"
+git config --global user.email "mg-build@mattgoucher.com"
+git config --global user.name "mg-build"
 
   standard-version patch -m "chore(release): version %s build ${CIRCLE_BUILD_NUM} [ci skip]"
 
