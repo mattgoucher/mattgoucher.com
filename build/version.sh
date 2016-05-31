e885680f94f7695c34cd5efca200ae903fbd42ed
mattgoucher <matt@mattgoucher.com>
2016-05-30 19:04:47 -0700

    .sh permissions

--- build/version.sh
+++ build/version.sh
@@ -5,5 +5,3 @@ git config --global user.name "mattgoucher"
   standard-version patch -m "chore(release): version %s build ${CIRCLE_BUILD_NUM} [ci skip]"
 
   git push --tags origin ${CIRCLE_BRANCH}
-
-  npm publish
